34525448763c4eee4cb8f459b1d623fbcdc40716
Well-0 <darian6388@gmail.com>
2025-11-03 12:11:11 +1100

    Added Production launch config

--- frontend/vite.config.ts
+++ frontend/vite.config.ts
@@ -6,5 +6,7 @@ export default defineConfig({
   build: {
     outDir: '../build/frontend',
     emptyOutDir: true,
+    minify: false,  // Disable minification to see readable code
+    sourcemap: true, // Generate source maps
   }
 })
